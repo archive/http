d6a7660238be930bcc5934ef67485aa2ee0b4e8a
Anders <joensson.anders@gmail.com>
2011-08-29 21:21:15 +0200

    init

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Yeah!');
-}).listen(80, "127.0.0.1");
-
+  res.end('Blaha Hello World\nApp (archive) is running..');
+}).listen(11635);
