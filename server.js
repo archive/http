22a1e26bf35558d8f32de02d3e130a2361876188
Anders <joensson.anders@gmail.com>
2011-08-30 19:50:41 +0200

    init

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var app = module.exports = express.createServer();
 var port = 11637;
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.set('view options', { layout: false });
+    app.set('views', __dirname + '/views');
+    //app.set('view engine', 'ejs');
+    app.set('view engine', 'jade');
+    app.set('view options', { layout: false });
 
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.get("/", function(request, response) {
